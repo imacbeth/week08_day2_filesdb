7545ed3afd835de0dc283642dfc75d092c775b14
Iona Macbeth <ionalmacbeth@gmail.com>
2018-07-10 15:57:16 +0100

    folder constructors

--- src/main/java/models/Folder.java
+++ src/main/java/models/Folder.java
@@ -1,7 +1,6 @@
 package models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
@@ -11,4 +10,19 @@ public class Folder {
     private int id;
     private String title;
     private List<File> files;
+
+    public Folder(String title) {
+        this.title = title;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
