42828f889537b10c98f067537cd9a96c48e68c4c
Iona Macbeth <ionalmacbeth@gmail.com>
2018-07-10 15:52:05 +0100

    file getters and setters

--- src/main/java/models/Folder.java
+++ src/main/java/models/Folder.java
@@ -1,9 +1,14 @@
 package models;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.util.List;
 
+@Entity
+@Table(name="folders")
 public class Folder {
 
+    private int id;
     private String title;
     private List<File> files;
 }
